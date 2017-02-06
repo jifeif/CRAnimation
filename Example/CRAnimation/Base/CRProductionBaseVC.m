733107fb4b662baca5d7d0098a2524ff01205cb7
BearRan <648070256@qq.com>
2017-02-06 23:13:31 +0800

    productionBaseVC继续搭建

--- Example/CRAnimation/Base/CRProductionBaseVC.m
+++ Example/CRAnimation/Base/CRProductionBaseVC.m
@@ -11,6 +11,8 @@
 @interface CRProductionBaseVC ()
 {
     UIButton    *_backBtn;
+    UIButton    *_shareBtn;
+    UIButton    *_detailsBtn;
     UILabel     *_productionTitleLabel;
     UIView      *_topBarView;
 }
@@ -25,38 +27,55 @@
     _backBtnColor = [UIColor whiteColor];
 }
 
+
+#pragma mark - createUI
+
 - (void)createUI
 {
     [super createUI];
-    
     [self addTopBarWithTitle:nil];
 }
 
 - (void)addTopBarWithTitle:(NSString *)title
+{
+    [self createTopBarView];
+    [self createTitleLabelWithTitle:title];
+    [self createBackBtn];
+    [self createShareBtn];
+    [self createDetailsBtn];
+    
+    [self relayTopBarContentsUI];
+}
+
+- (void)createTopBarView
 {
     if (!_topBarView) {
         _topBarView = [[UIView alloc] initWithFrame:CGRectMake(0, STATUS_HEIGHT, WIDTH, 40)];
         _topBarView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
         [self.view addSubview:_topBarView];
     }
-    
+}
+
+- (void)createTitleLabelWithTitle:(NSString *)title
+{
     if (!title) {
         title = @"";
     }
     if (!_productionTitleLabel) {
         _productionTitleLabel = [UILabel new];
         _productionTitleLabel.textColor = [UIColor whiteColor];
-        _productionTitleLabel.font = FontSize_6(12);
+        _productionTitleLabel.font = FontSize_6(18);
+        _productionTitleLabel.textAlignment = NSTextAlignmentCenter;
         [_topBarView addSubview:_productionTitleLabel];
     }
     _productionTitleLabel.text = title;
-    [_productionTitleLabel sizeToFit];
-    [_productionTitleLabel BearSetCenterToParentViewWithAxis:kAXIS_X_Y];
-    
-    
+}
+
+- (void)createBackBtn
+{
     if (!_backBtn) {
         
-        UIImage *backImage = [[UIImage imageNamed:@"back_icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        UIImage *backImage = [[UIImage imageNamed:@"nav_btn_close"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
         
         _backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         [_backBtn setImage:backImage forState:UIControlStateNormal];
@@ -64,22 +83,63 @@
         [_backBtn sizeToFit];
         [_backBtn addTarget:self action:@selector(popSelf) forControlEvents:UIControlEventTouchUpInside];
         [_topBarView addSubview:_backBtn];
-        [_backBtn BearSetRelativeLayoutWithDirection:kDIR_LEFT destinationView:nil parentRelation:YES distance:15 center:YES];
     }
 }
+
+- (void)createShareBtn
+{
+    if (!_shareBtn) {
+        _shareBtn = [UIButton new];
+        [_shareBtn setImage:[UIImage imageNamed:@"nav_btn_share1"] forState:UIControlStateNormal];
+        [_shareBtn sizeToFit];
+        [_shareBtn addTarget:self action:@selector(shareBtnEvent) forControlEvents:UIControlEventTouchUpInside];
+        [_topBarView addSubview:_shareBtn];
+    }
+}
+
+- (void)createDetailsBtn
+{
+    if (!_detailsBtn) {
+        _detailsBtn = [UIButton new];
+        [_detailsBtn setImage:[UIImage imageNamed:@"nav_btn_details"] forState:UIControlStateNormal];
+        [_detailsBtn sizeToFit];
+        [_detailsBtn addTarget:self action:@selector(detailBtnEvent) forControlEvents:UIControlEventTouchUpInside];
+        [_topBarView addSubview:_detailsBtn];
+    }
+}
+
+
+#pragma mark - relayUI
+
+- (void)relayTopBarContentsUI
+{
+    [_backBtn BearSetRelativeLayoutWithDirection:kDIR_LEFT destinationView:nil parentRelation:YES distance:XX_6(20) center:YES];
+    [_detailsBtn BearSetRelativeLayoutWithDirection:kDIR_RIGHT destinationView:nil parentRelation:YES distance:XX_6(20) center:YES];
+    [_shareBtn BearSetRelativeLayoutWithDirection:kDIR_LEFT destinationView:_detailsBtn parentRelation:NO distance:XX_6(15) center:YES];
+    
+    CGFloat titleLabelOffX = _topBarView.width - _shareBtn.x;
+    CGFloat titleLabelWidth = _topBarView.width - 2 * titleLabelOffX;
+    [_productionTitleLabel sizeToFit];
+    [_productionTitleLabel setWidth:titleLabelWidth];
+    [_productionTitleLabel BearSetCenterToParentViewWithAxis:kAXIS_X_Y];
+}
+
+
+#pragma mark - BtnEvent
 
 - (void)popSelf
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void)setBackBtnColor:(UIColor *)backBtnColor
+- (void)shareBtnEvent
 {
-    _backBtnColor = backBtnColor;
-    
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(7, (44-69/3.0)/2.0, 48/3.0, 69/3.0)];
-    [imageView setImage:[[UIImage imageNamed:@"share_iconShare"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
-    imageView.tintColor = [UIColor whiteColor];
+    NSLog(@"--shareBtnEvent");
+}
+
+- (void)detailBtnEvent
+{
+    NSLog(@"--detailBtnEvent");
 }
 
 - (void)didReceiveMemoryWarning {
