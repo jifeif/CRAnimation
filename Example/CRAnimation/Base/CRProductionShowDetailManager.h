ee89c7ff66a0765b4f1c614cb98b835576624735
BearRan <648070256@qq.com>
2017-02-08 08:43:58 +0800

    继续搭建详情页

--- Example/CRAnimation/Base/CRProductionShowDetailManager.h
+++ Example/CRAnimation/Base/CRProductionShowDetailManager.h
@@ -14,5 +14,6 @@
 + (CRProductionShowDetailManager *)commonManagerInVC:(__weak CRBaseViewController *)inVC;
 
 - (void)showDetailView;
+- (void)fadeDetailView;
 
 @end
--- Example/CRAnimation/Base/CRProductionShowDetailManager.m
+++ Example/CRAnimation/Base/CRProductionShowDetailManager.m
@@ -12,11 +12,20 @@
 #warning Test
 #import "CRCardAnimationViewDemoInfoModel.h"
 
+typedef enum {
+    kProductionDetailViewStatus_Idle,
+    kProductionDetailViewStatus_AlreadyShow,
+    kProductionDetailViewStatus_AlreadyFade,
+}ProductionDetailViewStatus;
+
 @interface CRProductionShowDetailManager ()
 
-@property (strong, nonatomic) CRBaseViewController    *inVC;
-@property (strong, nonatomic) UIView                  *maskBgView;
-@property (strong, nonatomic) CRProductionDetailView  *detailView;
+@property (strong, nonatomic) CRBaseViewController          *inVC;
+@property (strong, nonatomic) UIView                        *maskBgView;
+@property (strong, nonatomic) UIView                        *detailContentView;
+@property (strong, nonatomic) CRProductionDetailView        *detailView;
+@property (strong, nonatomic) UIButton                      *closeBtn;
+@property (assign, nonatomic) ProductionDetailViewStatus    viewStatus;
 
 @end
 
@@ -34,6 +43,7 @@
     
     if (self) {
         _inVC = inVC;
+        _viewStatus = kProductionDetailViewStatus_Idle;
         [self createMaskBgView];
         [self createDetailView];
     }
@@ -51,11 +61,25 @@
 
 - (void)createDetailView
 {
+    if (!_detailContentView) {
+        _detailContentView = [[UIView alloc] initWithFrame:_maskBgView.bounds];
+        [_maskBgView addSubview:_detailContentView];
+    }
+    
     if (!_detailView) {
-        
         CRCardAnimationViewDemoInfoModel *tempModel = [CRCardAnimationViewDemoInfoModel new];
         _detailView = [CRProductionDetailView commonDetailViewWithInfoModel:tempModel];
         _detailView.backgroundColor = [UIColor orangeColor];
+        [_detailContentView addSubview:_detailView];
+        [_detailView setMaxX:_detailContentView.width];
+    }
+    
+    if (!_closeBtn) {
+        _closeBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, YY_6N(56), 40, 40)];
+        [_closeBtn setImage:[UIImage imageNamed:@"animation_btn_withdraw"] forState:UIControlStateNormal];
+        [_closeBtn addTarget:self action:@selector(fadeDetailView) forControlEvents:UIControlEventTouchUpInside];
+        [_detailContentView addSubview:_closeBtn];
+        [_closeBtn setMaxX:_detailView.x - XX_6N(24)];
     }
 }
 
@@ -63,20 +87,46 @@
 {
     __weak typeof(self) weakSelf = self;
     
-    [_maskBgView removeFromSuperview];
+    BOOL enableShow = (_viewStatus == kProductionDetailViewStatus_Idle) || (_viewStatus == kProductionDetailViewStatus_AlreadyFade);
+    if (!enableShow) {
+        return;
+    }
+    
     _maskBgView.alpha = 0;
     [_inVC.view addSubview:_maskBgView];
     
-    [_detailView removeFromSuperview];
-    [_maskBgView addSubview:_detailView];
-    [_detailView setX:_maskBgView.width];
+    [_detailContentView setX:_maskBgView.width];
     
     [UIView animateWithDuration:0.3 animations:^{
         weakSelf.maskBgView.alpha = 1;
     }completion:^(BOOL finished) {
         
         [UIView animateWithDuration:0.3 animations:^{
-            [weakSelf.detailView setMaxX:_maskBgView.width];
+            [weakSelf.detailContentView setX:0];
+        }completion:^(BOOL finished) {
+            weakSelf.viewStatus = kProductionDetailViewStatus_AlreadyShow;
+        }];
+    }];
+}
+
+- (void)fadeDetailView
+{
+    __weak typeof(self) weakSelf = self;
+    
+    BOOL enableShow = (_viewStatus == kProductionDetailViewStatus_AlreadyShow);
+    if (!enableShow) {
+        return;
+    }
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [weakSelf.detailContentView setX:_maskBgView.width];
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:0.3 animations:^{
+            weakSelf.maskBgView.alpha = 0;
+        }completion:^(BOOL finished) {
+            [_maskBgView removeFromSuperview];
+            weakSelf.viewStatus = kProductionDetailViewStatus_AlreadyFade;
         }];
     }];
 }
