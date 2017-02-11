edbedb071caed9ec1ea0eeeb6fab22dbd1c1b29b
BearRan <648070256@qq.com>
2017-02-11 21:02:02 +0800

    继续编写成员界面

--- Example/CRAnimation/VCs/MembersVC/CRMembersVC.m
+++ Example/CRAnimation/VCs/MembersVC/CRMembersVC.m
@@ -13,6 +13,7 @@
 @interface CRMembersVC () <UITableViewDelegate, UITableViewDataSource>
 {
     UITableView *_mainTableView;
+    UIView      *_naviBarView;
 }
 
 @end
@@ -37,12 +38,46 @@
 
 - (void)createUI
 {
-    _mainTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, HEIGHT)];
+    self.navigationController.navigationBar.hidden = YES;
+    self.view.backgroundColor = color_323341;
+    
+    [self createNaviBarView];
+    [self createTableView];
+}
+
+- (void)createNaviBarView
+{
+    _naviBarView = [[UIView alloc] initWithFrame:CGRectMake(0, STATUS_HEIGHT, WIDTH, NAVIGATIONBAR_HEIGHT)];
+    _naviBarView.backgroundColor = color_323341;
+    [self.view addSubview:_naviBarView];
+    
+    UILabel *titleLabel = [UILabel new];
+    titleLabel.text = @"团队成员";
+    titleLabel.textColor = [UIColor whiteColor];
+    [titleLabel sizeToFit];
+    [_naviBarView addSubview:titleLabel];
+    [titleLabel BearSetCenterToParentViewWithAxis:kAXIS_X_Y];
+    
+    CGFloat shareBtn_width = _naviBarView.height;
+    UIButton *shareBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, shareBtn_width, shareBtn_width)];
+    [shareBtn setImage:[UIImage imageNamed:@"Nav_btn_share"] forState:UIControlStateNormal];
+    [shareBtn addTarget:self action:@selector(shareBtnEvent) forControlEvents:UIControlEventTouchUpInside];
+    [_naviBarView addSubview:shareBtn];
+    [shareBtn BearSetRelativeLayoutWithDirection:kDIR_RIGHT destinationView:nil parentRelation:YES distance:10 center:YES];
+}
+
+- (void)createTableView
+{
+    _mainTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, _naviBarView.maxY, WIDTH, HEIGHT - _naviBarView.maxY - TABBAR_HEIGHT)];
     _mainTableView.delegate = self;
     _mainTableView.dataSource = self;
     _mainTableView.backgroundColor = color_323341;
     _mainTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:_mainTableView];
+    
+    UIView *tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, WIDTH, YY_6N(50))];
+    tableHeaderView.backgroundColor = [UIColor clearColor];
+    _mainTableView.tableHeaderView = tableHeaderView;
 }
 
 
@@ -80,6 +115,15 @@
 {
     return 10;
 }
+
+
+#pragma mark - BtnEvent
+
+- (void)shareBtnEvent
+{
+    NSLog(@"--shareBtnEvent");
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
