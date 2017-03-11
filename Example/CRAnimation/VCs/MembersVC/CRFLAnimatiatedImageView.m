1d4d99705196ab17e0163e98a7426aa8e0600bb4
BearRan <648070256@qq.com>
2017-03-11 20:05:11 +0800

    修复SDWebImage问题

--- Example/CRAnimation/VCs/MembersVC/CRFLAnimatiatedImageView.m
+++ Example/CRAnimation/VCs/MembersVC/CRFLAnimatiatedImageView.m
@@ -14,17 +14,10 @@
 - (void)setGifImageWithURLStr:(NSString *)urlStr
 {
     __weak typeof(self) weakSelf = self;
-    
-    
-    // https://github.com/Flipboard/FLAnimatedImage
-    
-//    NSString *imagePath                = @"http://images2015.cnblogs.com/blog/607542/201601/607542-20160123090832343-133952004.gif";
-    NSData   *gifImageData             = [self imageDataFromDiskCacheWithKey:urlStr];
+    NSData *gifImageData = [self imageDataFromDiskCacheWithKey:urlStr];
     
     if (gifImageData) {
-        
         [self setGifImageWithGifImageData:gifImageData];
-        
     } else {
         
         NSURL *url = [NSURL URLWithString:urlStr];
@@ -33,8 +26,11 @@
                                                              progress:nil
             completed:^(UIImage *image, NSData *data, NSError *error, BOOL finished) {
                 
-                [[SDWebImageManager sharedManager] saveImageToCache:image
-                                                             forURL:url];
+                [[[SDWebImageManager sharedManager] imageCache] storeImage:image
+                                                                 imageData:data
+                                                                    forKey:url.absoluteString
+                                                                    toDisk:YES
+                                                                completion:nil];
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [weakSelf setGifImageWithGifImageData:data];
                 });
@@ -53,13 +49,5 @@
     NSString *path = [[[SDWebImageManager sharedManager] imageCache] defaultCachePathForKey:key];
     return [NSData dataWithContentsOfFile:path];
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
