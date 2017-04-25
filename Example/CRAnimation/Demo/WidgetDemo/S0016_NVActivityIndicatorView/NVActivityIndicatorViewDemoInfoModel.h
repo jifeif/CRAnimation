208c1fc19dc7492130a1d2895b07f582ce597d13
BearRan <648070256@qq.com>
2017-04-25 23:41:53 +0800

    新增S0016_NVActivityIndicatorView

---
+++ Example/CRAnimation/Demo/WidgetDemo/S0016_NVActivityIndicatorView/NVActivityIndicatorViewDemoInfoModel.h
@@ -0,0 +1,13 @@
+//
+//  NVActivityIndicatorViewDemoInfoModel.h
+//  CRAnimation
+//
+//  Created by Bear on 17/4/25.
+//  Copyright © 2017年 BearRan. All rights reserved.
+//
+
+#import "CRDemoInfoModel.h"
+
+@interface NVActivityIndicatorViewDemoInfoModel : CRDemoInfoModel
+
+@end
---
+++ Example/CRAnimation/Demo/WidgetDemo/S0016_NVActivityIndicatorView/NVActivityIndicatorViewDemoInfoModel.m
@@ -0,0 +1,41 @@
+//
+//  NVActivityIndicatorViewDemoInfoModel.m
+//  CRAnimation
+//
+//  Created by Bear on 17/4/25.
+//  Copyright © 2017年 BearRan. All rights reserved.
+//
+
+#import "NVActivityIndicatorViewDemoInfoModel.h"
+#import "CRAnimation_Example-swift.h"
+
+@implementation NVActivityIndicatorViewDemoInfoModel
+
+- (void)fillDemoInfo
+{
+    self.demoVCName     = @"_TtC19CRAnimation_Example29NVActivityIndicatorViewDemoVC";
+    self.demoGifName    = @"S0004_WCLLoadingView_20170420.gif";
+    self.demoType       = kCRDemoTypeStorage;
+    self.CRID           = @"S0016";
+    self.demoName       = @"NVActivityIndicatorView";
+    self.demoSummary    = @"各种实用的加载指示器";
+    self.codeLanguage   = kCRLanguageTypeSwift;
+    
+    self.originGitHubAddress            = @"https://github.com/ninjaprox/NVActivityIndicatorView";
+    self.homePage                       = @"http://blog.vinhis.me";
+}
+
+- (CRMemberInfoModel *)authorInfo
+{
+    CRMemberInfoModel *authorInfo = [CRMemberInfoModel new];
+    authorInfo.realName = @"";
+    authorInfo.nickName = @"Vinh Nguyen";
+    authorInfo.mail     = @"ninjaprox@gmail.com";
+    authorInfo.qq       = @"";
+    authorInfo.homePage = @"http://blog.vinhis.me";
+    authorInfo.professionType = kProfessionTypeiOSDeveloper;
+    
+    return authorInfo;
+}
+
+@end
